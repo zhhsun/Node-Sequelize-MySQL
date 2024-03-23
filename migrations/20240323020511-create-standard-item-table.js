05aa0e3be29596815ee96f7b35217bb11caa78e9
Mirabelle <mirabelle@MirabelledeMacBook-Pro.local>
2024-03-23 10:34:20 +0800

    Add mig script and model

---
+++ migrations/20240323020511-create-standard-item-table.js
@@ -0,0 +1,82 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('standard_items', {
+      id: {
+        type: Sequelize.DataTypes.UUID,
+        allowNull: false,
+      },
+      _id: {
+        type: Sequelize.DataTypes.BIGINT,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      parent_id: {
+        type: Sequelize.DataTypes.BIGINT,
+        allowNull: true,
+      },
+      name: {
+        type: Sequelize.DataTypes.STRING,
+        length: 127,
+        allowNull: true,
+      },
+      type: {
+        type: Sequelize.DataTypes.STRING,
+        length: 127,
+        allowNull: true,
+      },
+      count: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: true,
+        defaultValue: 0
+      },
+      state: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: true,
+      },
+      purchase_order_id: {
+        type: Sequelize.DataTypes.BIGINT,
+        allowNull: true,
+      },
+      supplier_id: {
+        type: Sequelize.DataTypes.BIGINT,
+        allowNull: true,
+      },
+      required_at: {
+        type: Sequelize.DataTypes.DATE,
+        allowNull: false,
+      },
+      note: {
+        type: Sequelize.DataTypes.TEXT,
+        allowNull: true,
+      },
+      deleted: {
+        type: Sequelize.DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      created_at: {
+        type: Sequelize.DataTypes.DATE,
+        defaultValue: new Date(),
+      },
+      created_by: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: true,
+      },
+      updated_at: {
+        type: Sequelize.DataTypes.DATE,
+        defaultValue: new Date(),
+      },
+      updated_by: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: true,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('standard_items');
+  },
+};
---
+++ models/standardItems.js
@@ -0,0 +1,95 @@
+'use strict';
+
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class StandardItems extends Model {
+    static associate(models) {}
+  }
+
+  StandardItems.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+      _id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      parentId: {
+        type: DataTypes.BIGINT,
+        allowNull: true,
+        field: 'parent_id'
+      },
+      name: {
+        type: DataTypes.STRING,
+        length: 127,
+        allowNull: true,
+      },
+      type: {
+        type: DataTypes.STRING,
+        length: 127,
+        allowNull: true,
+      },
+      count: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        defaultValue: 0
+      },
+      state: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      purchaseOrderId: {
+        type: DataTypes.BIGINT,
+        allowNull: true,
+        field: 'purchase_order_id'
+      },
+      supplierId: {
+        type: DataTypes.BIGINT,
+        allowNull: true,
+        field: 'supplier_id'
+      },
+      requiredAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        field: 'required_at'
+      },
+      note: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      deleted: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: new Date(),
+        field: 'created_at',
+      },
+      createdBy: {
+        type: DataTypes.STRING,
+        field: 'created_by',
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: new Date(),
+        field: 'updated_at',
+      },
+      updatedBy: {
+        type: DataTypes.STRING,
+        field: 'updated_by',
+      },
+    },
+    {
+      sequelize,
+      modelName: 'standard_items',
+    }
+  );
+
+  return StandardItems;
+};
